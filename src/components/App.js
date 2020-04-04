2c86d99a44a82b895e1132f3086da17c7f2c295e
Dmitrij Prokhorenko <dmitrij.prokhorenko@gmail.com>
2020-04-04 23:35:11 +0300

    hard start of homework

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import Profile from './Profile/Profile';
+import ProfileData from './Profile/user.json';
+
+ReactDOM.render(
+  <>
+    <Profile data={ProfileData} />
+  </>,
+  document.getElementById('root'),
+);
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,2 +1,59 @@
 import React from 'react';
-import reactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+import styles from './Profile.module.css';
+
+const Profile = ({
+  data: {
+    name,
+    tag,
+    location,
+    avatar,
+    stats: { followers, views, likes },
+  },
+}) => (
+  <div className={styles.wrap}>
+    <div className={styles.description}>
+      <img src={avatar} alt={name} className={styles.image} />
+      <p className={styles.name}>{name}</p>
+      <p className={styles.tag}>{tag}</p>
+      <p className={styles.location}>{location}</p>
+    </div>
+
+    <ul className={styles.list}>
+      <li className={styles.listItem}>
+        <span className={styles.label}>Followers</span>
+        <span className={styles.quantity}> {followers}</span>
+      </li>
+      <li className={styles.listItem}>
+        <span className={styles.label}>Views</span>
+        <span className={styles.quantity}> {views}</span>
+      </li>
+      <li className={styles.listItem}>
+        <span className={styles.label}>Likes</span>
+        <span className={styles.quantity}> {likes}</span>
+      </li>
+    </ul>
+  </div>
+);
+
+Profile.defaultProps = {
+  data: {
+    location: 'N/A',
+    avatar: 'https://theatreonpodol.com/wp-content/themes/main/img/noimage.jpg',
+    stats: { followers: 0, views: 0, likes: 0 },
+  },
+};
+
+Profile.propTypes = {
+  data: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string,
+    avatar: PropTypes.string,
+    followers: PropTypes.number,
+    likes: PropTypes.number,
+    views: PropTypes.number,
+  }).isRequired,
+};
+
+export default Profile;
