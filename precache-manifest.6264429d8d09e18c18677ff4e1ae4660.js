9a8a9cd4cd9d373d8a4cd02937dae977cc03e1dd
Dmitrij Prokhorenko <dmitrij.prokhorenko@gmail.com>
2020-07-04 20:25:23 +0300

    Updates

---
+++ precache-manifest.6264429d8d09e18c18677ff4e1ae4660.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59408d893838ff2e973e5c8991cbfb41",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "f69c560658017a31ebf5",
+    "url": "/goit-react-hw-01-components/static/css/main.e8a285f2.chunk.css"
+  },
+  {
+    "revision": "f66c7242890b3905d8e2",
+    "url": "/goit-react-hw-01-components/static/js/2.03f0f547.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.03f0f547.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f69c560658017a31ebf5",
+    "url": "/goit-react-hw-01-components/static/js/main.0ed56818.chunk.js"
+  },
+  {
+    "revision": "61ea0416005e4a073e74",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.60be8746.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-01-components/precache-manifest.a492d8f253ec7f662ca4dae705b3387d.js"
+  "/goit-react-hw-01-components/precache-manifest.6264429d8d09e18c18677ff4e1ae4660.js"
 );
 
 self.addEventListener('message', (event) => {
