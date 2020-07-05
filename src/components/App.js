36770b7726de93da707421340f2e1e66f8b220b7
Dmitrij Prokhorenko <dmitrij.prokhorenko@gmail.com>
2020-07-05 17:04:42 +0300

    recustomized project structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 import Profile from './Profile/Profile';
 import Statistic from './Statistic/Statistic';
@@ -11,18 +10,21 @@ import Statistics from '../data/statistical-data.json';
 import Friends from '../data/friends.json';
 import Transactions from '../data/transactions.json';
 
-ReactDOM.render(
-  <>
-    <Profile
-      name={Users.name}
-      tag={Users.tag}
-      avatar={Users.avatar}
-      location={Users.location}
-      stats={Users.stats}
-    />
-    <Statistic title="Download statistic" stats={Statistics} />
-    <FriendList friends={Friends} />
-    <TransacrionHistory transactions={Transactions} />
-  </>,
-  document.getElementById('root'),
-);
+const App = () => {
+  return (
+    <>
+      <Profile
+        name={Users.name}
+        tag={Users.tag}
+        avatar={Users.avatar}
+        location={Users.location}
+        stats={Users.stats}
+      />
+      <Statistic title="Download statistic" stats={Statistics} />
+      <FriendList friends={Friends} />
+      <TransacrionHistory transactions={Transactions} />
+    </>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
 import './global.css';
-import './components/App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
